8b1d3aab622bfd1baf5ac7a6ab1c2474569f7dcb
Alix Muller <amuller@c1r5s3.42seoul.kr>
2024-01-26 16:58:00 +0900

    Rust Sully pretty close

--- Rust/Sully/src/main.rs
+++ Rust/Sully/src/main.rs
@@ -2,6 +2,12 @@ use std::process::Command;
 use std::fs::File;
 use std::io::Write;
 
+const SOURCE_CODE: &str = r##"use std::process::Command;
+use std::fs::File;
+use std::io::Write;
+
+const SOURCE_CODE: &str = r#""#;
+
 fn main() {
     let i = 5;
     let name = format!("Sully_{}", i);
@@ -15,12 +21,50 @@ fn main() {
     .status()
     .expect("Failed to build the project");
 
-    let data = format!("fn main() {{ println!(\"Meooooow\"); let i = {}; }}", i - 1);
-    let mut file = File::create(format!("{}/src/main.rs", name)).expect("Unable to open file");
+    let data = format!("use std::process::Command;
+    use std::fs::File;
+    use std::io::Write;
+    
+    const SOURCE_CODE: &str = r{}#\"{}\"#{};
+
+{}", '#', SOURCE_CODE, '#', &SOURCE_CODE[101..]);
+    let mut file = File::create(format!("../{}/src/main.rs", name)).expect("Unable to open file");
     file.write_all(data.as_bytes()).expect("Unable to write data");
 
     Command::new("cargo")
-        .current_dir(name)
+        .current_dir("../".to_string() + &name)
+        .arg("run")
+        .status()
+        .expect("Failed to run the project");
+}
+"##;
+
+fn main() {
+    let i = 5;
+    let name = format!("Sully_{}", i);
+
+    if i <= 0 {
+        return;
+    }
+
+    Command::new("cargo")
+    .current_dir("../")
+    .args(&["new", "--bin", name.as_str()])
+    .status()
+    .expect("Failed to build the project");
+
+    let data = format!("use std::process::Command;
+    use std::fs::File;
+    use std::io::Write;
+    
+    const SOURCE_CODE: &str = r{}#\"{}\"#{};
+
+{}", '#', SOURCE_CODE, '#', &SOURCE_CODE[101..]);
+    let mut file = File::create(format!("../{}/src/main.rs", name)).expect("Unable to open file");
+    file.write_all(data.as_bytes()).expect("Unable to write data");
+
+    Command::new("cargo")
+        .current_dir("../".to_string() + &name)
         .arg("run")
         .status()
         .expect("Failed to run the project");
