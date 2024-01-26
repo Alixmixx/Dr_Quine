1d3f4c2d80231c788854b7e1ee708269f3931501
Alix Muller <amuller@c1r5s3.42seoul.kr>
2024-01-26 15:27:15 +0900

    Rust Sully not bad

--- Rust/Sully/src/main.rs
+++ Rust/Sully/src/main.rs
@@ -1,13 +1,26 @@
 use std::process::Command;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
-    Command::new("cargo")
-        .args(&["new", "--bin", "Sully1"])
-        .status()
-        .expect("Failed to build the project");
+    let i = 5;
+    let name = format!("Sully_{}", i);
+
+    if i <= 0 {
+        return;
+    }
 
     Command::new("cargo")
-        .current_dir("Sully1")
+    .args(&["new", "--bin", name.as_str()])
+    .status()
+    .expect("Failed to build the project");
+
+    let data = format!("fn main() {{ println!(\"Meooooow\"); let i = {}; }}", i - 1);
+    let mut file = File::create(format!("{}/src/main.rs", name)).expect("Unable to open file");
+    file.write_all(data.as_bytes()).expect("Unable to write data");
+
+    Command::new("cargo")
+        .current_dir(name)
         .arg("run")
         .status()
         .expect("Failed to run the project");
