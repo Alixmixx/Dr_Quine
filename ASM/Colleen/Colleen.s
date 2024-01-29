9f0be44cd0c24f20af41b4e1cbae6840f0ee4007
amuller <amuller@student.42seoul.kr>
2024-01-29 12:59:03 +0900

    not bad need cleanup

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,15 +1,20 @@
+; AMS Quine
 section .text
     global _main
     extern _printf
 
 _main:
-    push rbp            ;save base pointer  
-    mov rbp, rsp        ;create stack frame
-    lea rdi, [rel msg]  ;load address of msg into rdi
-    call _printf        ;call printf
-    pop rbp             ;restore base pointer
-    xor rax, rax        ;clear eax
-    ret                 ;return
+    push rbp
+    mov rbp, rsp
+    lea rdi, [rel msg]
+    lea rsi, [rel msg]
+    mov rdx, 10
+    mov rcx, 34
+    mov r8, 9
+    call _printf
+    pop rbp
+    xor rax, rax
+    ret
 
 section .data
-    msg db "Hello, world!", 0    
+    msg db "; AMS Quine%2$csection .text%2$c%4$cglobal _main%2$c%4$cextern _print%2$c%2$c_main:%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$clea rdi, [rel msg]%2$c%4$clea rsi, [rel msg]%2$c%4$cmov rdx, 10%2$c%4$cmov rcx, 34%2$c%4$cmov r8, 9%2$c%4$ccall _printf%2$c%4$cpop rbp%2$c%4$cxor rax, rax%2$c%4$cret%2$c%2$csection .data%2$c%4$cmsg db %3$c%1$s%3$c, 0", 0
