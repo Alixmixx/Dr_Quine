4df3b8c22ca0416f32889611505f0b9b7731bd82
amuller <amuller@student.42seoul.kr>
2024-02-07 15:02:26 +0900

    ASM Grace fix and Sully start

--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -7,7 +7,7 @@ section .text
 	global _main
 	extern _dprintf
 
-_main:
+MAIN:
 	push rbp
 	mov rbp, rsp
 	call _open
@@ -42,4 +42,4 @@ _exit:
 
 section .data
 	filename: db "Grace_kid.s", 0
-	msg: db ";Quine%2$c%%define MAIN _main%2$c%%define OPEN_CALL_NB 0x2000005%2$c%%define CLOSE_CALL_NB 0x2000006%2$c%2$csection .text%2$c%4$cglobal _main%2$c%4$cextern _dprintf%2$c%2$c_main:%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$ccall _open%2$c%4$ccall _exit%2$c%2$c_open:%2$c%4$cmov eax, OPEN_CALL_NB%2$c%4$clea rdi, [rel filename]%2$c%4$cmov rsi, 0x202%2$c%4$cmov rdx, 420%2$c%4$csyscall%2$c%2$c_write:%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$cmov rdi, rax%2$c%4$clea rsi, [rel msg]%2$c%4$clea rdx, [rel msg]%2$c%4$cmov rcx, 10%4$c%4$c%4$c; new line%2$c%4$cmov r8, 34%4$c%4$c%4$c; double quote%2$c%4$cmov r9, 9%4$c%4$c%4$c; tab%2$c%4$ccall _dprintf%2$c%2$c_close:%2$c%4$cmov eax, CLOSE_CALL_NB%2$c%4$csyscall%2$c%2$c_exit:%2$c%4$cleave%2$c%4$cret%2$c%2$c%2$csection .data%2$c%4$cfilename: db %3$cGrace_kid.s%3$c, 0%2$c%4$cmsg: db %3$c%1$s%3$c, 0", 0
+	msg: db ";Quine%2$c%%define MAIN _main%2$c%%define OPEN_CALL_NB 0x2000005%2$c%%define CLOSE_CALL_NB 0x2000006%2$c%2$csection .text%2$c%4$cglobal _main%2$c%4$cextern _dprintf%2$c%2$cMAIN:%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$ccall _open%2$c%4$ccall _exit%2$c%2$c_open:%2$c%4$cmov eax, OPEN_CALL_NB%2$c%4$clea rdi, [rel filename]%2$c%4$cmov rsi, 0x202%2$c%4$cmov rdx, 420%2$c%4$csyscall%2$c%2$c_write:%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$cmov rdi, rax%2$c%4$clea rsi, [rel msg]%2$c%4$clea rdx, [rel msg]%2$c%4$cmov rcx, 10%4$c%4$c%4$c; new line%2$c%4$cmov r8, 34%4$c%4$c%4$c; double quote%2$c%4$cmov r9, 9%4$c%4$c%4$c; tab%2$c%4$ccall _dprintf%2$c%2$c_close:%2$c%4$cmov eax, CLOSE_CALL_NB%2$c%4$csyscall%2$c%2$c_exit:%2$c%4$cleave%2$c%4$cret%2$c%2$c%2$csection .data%2$c%4$cfilename: db %3$cGrace_kid.s%3$c, 0%2$c%4$cmsg: db %3$c%1$s%3$c, 0", 0
--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -1,32 +1,64 @@
+;Quine
 section .text
 	global _main
-	extern _printf
+	extern _dprintf
+	extern _system
 
 _main:
+	push rbp
+	mov rbp, rsp
+	call _checknum
+	call _exit
 
-_check_num:
-
+_checknum:
+	;if (filenum < 0 ) exit
 
 _open:
-	mov eax, 5
-	mov ebx, filename
-	mov ecx, 0
-	int 0x80
-	mov fd, eax
+	mov eax, 0x2000005 			; open syscall
+	lea rdi, [rel source]
+	mov rsi, 0x202
+	mov rdx, 420
+	syscall
 
-_write_to_file:
-	mov eax, 4
-	mov ebx, fd
-	mov ecx, buffer
-	mov edx, 100
-	int 0x80
+_write:
+	push rbp
+	mov rbp, rsp
+	mov rdi, rax
+	lea rsi, [rel msg]
+	lea rdx, [rel msg]
+	mov rcx, 10			; new line
+	mov r8, 34			; double quote
+	mov r9, 9			; tab
+	call _dprintf
 
 _close:
-	mov eax, 6
-	mov ebx, fd
-	int 0x80
+	mov eax, 0x2000006			; close syscall
+	syscall
+
+_create_command:
+	lea rdi, [rel command]
+	lea rsi, [rel command_template]
+	lea rsi, [rel source]
+	lea rdx, [rel object]
+	lea r8, [rel program]
+	
+
+_execute:
+	lea rdi, [rel command]
+	call _system
 
 _exit:
-	mov eax, 1
-	mov ebx, 0
-	int 0x80
+	leave
+	ret
+
+section .data
+	filename: db "Sully_%d.%c", 0
+	outname: db "Sully_%d", 0
+	command_template: db "nasm -f macho64 %1$s -o %2$s && gcc -o %3$s %2$s && ./%3$s", 0 
+	msg: db ";Quine%2$c%%define MAIN _main%2$c%%define OPEN_CALL_NB 0x2000005%2$c%%define CLOSE_CALL_NB 0x2000006%2$c%2$csection .text%2$c%4$cglobal _main%2$c%4$cextern _dprintf%2$c%2$c_main:%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$ccall _open%2$c%4$ccall _exit%2$c%2$c_open:%2$c%4$cmov eax, OPEN_CALL_NB%2$c%4$clea rdi, [rel filename]%2$c%4$cmov rsi, 0x202%2$c%4$cmov rdx, 420%2$c%4$csyscall%2$c%2$c_write:%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$cmov rdi, rax%2$c%4$clea rsi, [rel msg]%2$c%4$clea rdx, [rel msg]%2$c%4$cmov rcx, 10%4$c%4$c%4$c; new line%2$c%4$cmov r8, 34%4$c%4$c%4$c; double quote%2$c%4$cmov r9, 9%4$c%4$c%4$c; tab%2$c%4$ccall _dprintf%2$c%2$c_close:%2$c%4$cmov eax, CLOSE_CALL_NB%2$c%4$csyscall%2$c%2$c_exit:%2$c%4$cleave%2$c%4$cret%2$c%2$c%2$csection .data%2$c%4$cfilename: db %3$cGrace_kid.s%3$c, 0%2$c%4$cmsg: db %3$c%1$s%3$c, 0", 0
+	
+	command: resb 100
+	source: resb 15
+	object: resb 15
+	program: resb 15
+	filenum: resb 4
