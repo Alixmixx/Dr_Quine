245e8f585452dfa897b46658fef6ad68d3578820
amuller <amuller@student.42seoul.kr>
2024-02-08 15:40:33 +0900

    ASM Colleen comment fix

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -8,13 +8,13 @@ _main:
 	mov rbp, rsp
 	lea rdi, [rel msg]
 	lea rsi, [rel msg]
-	mov rdx, 10			; new line
-	mov rcx, 34			; double quote
-	mov r8, 9			; tab
+	mov rdx, 10 ; new line
+	mov rcx, 34 ; double quote
+	mov r8, 9 ; tab
 	call _printf ; extern printf call
 	pop rbp
 	xor rax, rax
 	ret
 
 section .data
-	msg db "; AMS Quine%2$csection .text%2$c%4$cglobal _main%2$c%4$cextern _printf%2$c%2$c_main:%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$clea rdi, [rel msg]%2$c%4$clea rsi, [rel msg]%2$c%4$cmov rdx, 10%2$c%4$cmov rcx, 34%2$c%4$cmov r8, 9%2$c%4$ccall _printf ; extern printf call%2$c%4$cpop rbp%2$c%4$cxor rax, rax%2$c%4$cret%2$c%2$csection .data%2$c%4$cmsg db %3$c%1$s%3$c, 0", 0
+	msg db "; AMS Quine%2$csection .text%2$c%4$cglobal _main%2$c%4$cextern _printf%2$c%2$c_main:%2$c%4$cpush rbp%2$c%4$cmov rbp, rsp%2$c%4$clea rdi, [rel msg]%2$c%4$clea rsi, [rel msg]%2$c%4$cmov rdx, 10 ; new line%2$c%4$cmov rcx, 34 ; double quote%2$c%4$cmov r8, 9 ; tab%2$c%4$ccall _printf ; extern printf call%2$c%4$cpop rbp%2$c%4$cxor rax, rax%2$c%4$cret%2$c%2$csection .data%2$c%4$cmsg db %3$c%1$s%3$c, 0", 0
